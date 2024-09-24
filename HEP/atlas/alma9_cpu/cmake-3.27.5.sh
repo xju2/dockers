74b395c6e95a678d9fd6d2aadf9666efe8ce5984
Xiangyang Ju <xju@lbl.gov>
2024-09-23 23:04:55 -0700

    update alma9

---
+++ HEP/atlas/alma9_cpu/cmake-3.27.5.sh
@@ -0,0 +1,6 @@
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
+#
+# Environment setup for CMake 3.27.5.
+#
+
+export PATH=/opt/cmake/3.27.5/Linux-x86_64/bin${PATH:+:${PATH}}
---
+++ HEP/atlas/alma9_cpu/etc/ninja-1.11.1.sh
@@ -0,0 +1,6 @@
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
+#
+# Environment setup for Ninja 1.11.1.
+#
+
+export PATH=/opt/ninja/1.11.1/Linux-x86_64${PATH:+:${PATH}}
