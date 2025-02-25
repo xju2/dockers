f398e343b47527e703b497cf0e599119bc3ec5f6
Xiangyang Ju <xju@lbl.gov>
2025-02-25 12:43:49 -0800

    remove old

--- HEP/atlas-GPU/centos7/etc/ninja-1.10.2.sh
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
-#
-# Environment setup for Ninja 1.10.2.
-#
-
-export PATH=/opt/ninja/1.10.2/Linux-x86_64${PATH:+:${PATH}}
-
--- HEP/atlas-GPU/cmake-3.21.3.sh
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
-#
-# Environment setup for CMake 3.21.3.
-#
-
-export PATH=/opt/cmake/3.21.3/Linux-x86_64/bin${PATH:+:${PATH}}
