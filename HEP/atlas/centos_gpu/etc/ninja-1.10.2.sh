602ae84352fb351b749d9e7267157c5f5f686b83
Xiangyang Ju <xju@lbl.gov>
2025-02-25 12:13:58 -0800

    rename

---
+++ HEP/atlas/centos_gpu/etc/ninja-1.10.2.sh
@@ -0,0 +1,7 @@
+# Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+#
+# Environment setup for Ninja 1.10.2.
+#
+
+export PATH=/opt/ninja/1.10.2/Linux-x86_64${PATH:+:${PATH}}
+
