71ca6855417dd6fec39ba6e2793e008e74e75ad8
Xiangyang Ju <xju@lbl.gov>
2025-04-28 15:16:37 -0700

    more atlas scripts

---
+++ HEP/atlas/elma9_gpu/atlas_prompt.sh
@@ -0,0 +1,9 @@
+#
+# Tweaks to the prompt seen by the user in interactive mode.
+#
+
+# Set up a nice prompt:
+export PS1='\[\033[01;35m\][bash]\[\033[01;31m\][\u]\[\033[01;34m\]:\W >\[\033[00m\] ';
+
+# Enable color output for the terminal:
+export CLICOLOR=1
